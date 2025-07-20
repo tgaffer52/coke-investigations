131cc0ba475291f154e53c7756f35f1fa84915cb
Trevor Gaffney <tgaffer@gmail.com>
2025-07-20 11:23:47 +0900

    reconfigure vuetify

--- nuxt-3-app/nuxt.config.ts
+++ nuxt-3-app/nuxt.config.ts
@@ -1,34 +1,49 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
+  build: {
+    transpile: ["vuetify"],
+  },
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "vuetify-nuxt-module", "@nuxt/content"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    (_options, nuxt) => {
+      nuxt.hooks.hook("vite:extendConfig", (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }));
+      });
+    },
+    ,
+    "@nuxt/content",
+  ],
   tailwindcss: {
     exposeConfig: true,
     viewer: true,
     // and more...
   },
-  vuetify: {
-    moduleOptions: {
-      /* module specific options */
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
     },
-    vuetifyOptions: {
-      theme: {
-        defaultTheme: "dark",
-        themes: {
-          dark: {
-            dark: true,
-            colors: {
-              background: "#121212",
-              surface: "#1E1E1E",
-              primary: "#BB86FC",
-              secondary: "#03DAC6",
-              error: "#CF6679",
-            },
+  },
+  vuetify: {
+    theme: {
+      defaultTheme: "dark",
+      themes: {
+        dark: {
+          dark: true,
+          colors: {
+            background: "#121212",
+            surface: "#1E1E1E",
+            primary: "#BB86FC",
+            secondary: "#03DAC6",
+            error: "#CF6679",
           },
         },
       },
-      /* vuetify options */
     },
   },
 });
